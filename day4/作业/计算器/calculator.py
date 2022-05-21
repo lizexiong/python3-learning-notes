2aaf1e3896c7398b98d8f85993cb4a72ec7e9b0a
lizexiong <1043460476@qq.com>
2022-05-21 20:53:28 +0800

    day4 homework start v2

--- day4/作业/计算器/calculator.py
+++ day4/作业/计算器/calculator.py
@@ -1,1 +1,46 @@
 __author__ = 'Administrator'
+
+
+
+
+
+import re
+
+UserInput = """1 - 2 * ( (60-30 +(-40/5) * (9-2*5/3 + 7 /3*99/4*2998 +10 * 568/14 )) - (-4*3)/ (16-3*2) )"""
+
+
+# str = re.findall('\+|\-|\*|\/|\(|\)|\d+',UserInput)
+
+
+ErrorStr=[]
+LeftBracket = []
+RightBracket = []
+for singlestr in UserInput:
+    str = re.search('\+|\-|\*|\/|\(|\)|\d+| ',singlestr)
+    if str==None:
+        ErrorStr.append(singlestr)
+    elif str.group() == '(':
+        LeftBracket.append(str.group())
+    elif str.group() == ')':
+        RightBracket.append(str.group())
+
+
+if len(ErrorStr) > 0 :
+    print ("有错误字符,错误字符有以下:",ErrorStr)
+elif len(LeftBracket) > len(RightBracket):
+    print ("多了一个左括号")
+elif len(LeftBracket) < len(RightBracket):
+    print ("多了一个右括号")
+
+
+
+
+
+
+
+
+
+
+
+
+
