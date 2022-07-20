d723c031838533ee9e3b733c4bfc850cdf2f9302
Zexiong Li <zexiong.li@dena.local>
2022-07-20 20:42:13 +0800

    day5 homework w7 2022-07-20-20:42:08

--- day5/作业/购物车升级版本+ATM系统独立/taobao/modules/StoreHandler.py
+++ day5/作业/购物车升级版本+ATM系统独立/taobao/modules/StoreHandler.py
@@ -7,16 +7,13 @@ from taobao.dbhelper.StoreDBHandler import StoreProductList,StoreUserInfo
 from bank.dbhelper.BankDBHandler import BankUserInfo
 from bank.modules.BankLogicHandler import CloseAnAccount               #不能再调用了，不然就调用循环了
 
-#用户购物车,可以写入本地，但是为了测试，需要每次重启程序后不保存，所以写成一个全局变量
-
-UserShoppingTrolley=dict()
-
 
 
 #购买和退订商品
 def Buy(num,user,inc=None):
     UserShoppingTrolleyTmp = dict()
     ProductList = StoreProductList()
+    print (ProductList)
     if ProductList.get(num):
         FromDBBankUserInfo = BankUserInfo()
         FromDBStoreUserInfo = StoreUserInfo()
@@ -24,18 +21,19 @@ def Buy(num,user,inc=None):
         ProductPrice = ProductList[num]['price']
         ProductName = ProductList[num]['name']
         UserShoppingCart = eval(str(FromDBStoreUserInfo[user]['usershoppingcart']))
-        #print (UserShoppingCart)
         if inc == 'delete':
             while True:
                 try:
                     DelProductNum = int(input("要删除的商品数量"))
-                    if UserShoppingTrolley[num]['buy'] >= DelProductNum:
-                        if UserShoppingTrolley[num]['buy'] <=1:
-                            UserShoppingTrolley.pop(num)
-                            ProductList[num]['num'] += 1
+                    if UserShoppingCart[num]['buy'] >= DelProductNum:
+                        if UserShoppingCart[num]['buy'] <=1:
+                            UserShoppingCart.pop(num)
+                            ProductNum = ProductList[num]['num'] + 1
                         else:
-                            ProductList[num]['num'] = ProductList[num]['num'] + DelProductNum
-                            UserShoppingTrolley[num]['buy'] = UserShoppingTrolley[num]['buy'] - DelProductNum
+                            UserShoppingCart[num]['buy'] = UserShoppingCart[num]['buy'] - DelProductNum
+                            ProductNum = ProductList[num]['num'] + DelProductNum
+                        StoreUserInfo('write', user, 'usershoppingcart', UserShoppingCart)
+                        StoreProductList('write',num,'num',ProductNum)
                         break
                     else:
                         print("您的购物车没有这么多商品，请重新输入")
@@ -78,18 +76,21 @@ def Buy(num,user,inc=None):
 def ShoppingTrolley(user):
     #os.system('clean')
     print (" *************************************欢迎进入购物车**************************************")
-    if UserShoppingTrolley:                                                                                                                  #如果购物车列表里面存在商品才会展示购物车
+    ProductList = StoreProductList()
+    FromDBStoreUserInfo = StoreUserInfo()
+    UserShoppingCart = eval(str(FromDBStoreUserInfo[user]['usershoppingcart']))
+    if UserShoppingCart:                                                                                                                  #如果购物车列表里面存在商品才会展示购物车
         print('%-4s %-5s  %-15s  %-10s  %-10s  %-10s' % (' ','编号','商品名称','商品价格(元)','商品剩余数量(个)','商品已买数量(个)'))
-        for k,v in UserShoppingTrolley.items():                                                                                              #开始循环购物车
-            print('%-5s %-5s  %-20s  %-20d  %-15d  %-10d' % (' ',k,UserShoppingTrolley[k]['name'],int(UserShoppingTrolley[k]['price']),int(ProductList[k]['num']),int(UserShoppingTrolley[k]['buy'])))      #这里的总商品数用的ProductList的，BuyShoppingTrolley字典没有存储总商品数量
+        for k,v in UserShoppingCart.items():                                                                                              #开始循环购物车
+            print('%-5s %-5s  %-20s  %-20d  %-15d  %-10d' % (' ',k,UserShoppingCart[k]['name'],int(UserShoppingCart[k]['price']),int(ProductList[k]['num']),int(UserShoppingCart[k]['buy'])))      #这里的总商品数用的ProductList的，BuyShoppingTrolley字典没有存储总商品数量
 
         UserInc = input("请选择菜单 ：是否删除和新增商品,请输入'delete'或者'add',或者按'b'进行结算,否者返回主界面输入'q'")               #让用户选择操作
         if UserInc == "delete":                                                                                             #接下来看是删除还是增加商品，那么就会调用Buy()函数
-            ProductID = int(input("请输入商品编号"))
+            ProductID = input("请输入商品编号")
             Buy(ProductID,user,UserInc)
             ShoppingTrolley(user)
         elif UserInc == 'add':
-            ProductID = int(input("请输入商品编号"))
+            ProductID = input("请输入商品编号")
             Buy(ProductID,user)
             ShoppingTrolley(user)
         elif UserInc == 'b':
