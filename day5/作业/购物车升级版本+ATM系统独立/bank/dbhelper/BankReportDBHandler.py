f2c0e60fa701e79f5460f5be33523fa31bb86473
lizexiong <1043460476@qq.com>
2022-07-27 00:52:18 +0800

    day5 homework w7 2022-07-27-00:52

--- day5/作业/购物车升级版本+ATM系统独立/bank/dbhelper/BankReportDBHandler.py
+++ day5/作业/购物车升级版本+ATM系统独立/bank/dbhelper/BankReportDBHandler.py
@@ -3,11 +3,27 @@
 
 
 
-
+import json
 
 
 def TransactionRecord(action=None,time=None,user=None,num=None,type=None,product=None,price=None,buy=None):
-    if
+    with open('../databases/BankReportDB', 'r+') as db_read, open('../databases/BankReportDBBak', 'w+') as db_write:
+        if action == "read":
+            if type == None:
+                pass
+            elif type == "bill":
+                pass
+            elif type == "atm":
+                pass
+        elif action == "write":
+            try:
+                ALLDbInfo = json.loads(db_read.read())[user]
+            except:
+                ALLDbInfo = {user:{'num':num,'type':type,'product':product,'price':price,'buy':buy}}
+            print (ALLDbInfo)
+            #ALLDbInfo[user][num] = num
+            db_write.write(json.dumps(ALLDbInfo))
 
 
+TransactionRecord(action='write',user='lizexiong')
  
