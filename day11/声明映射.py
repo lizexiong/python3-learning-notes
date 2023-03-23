ef260df9f7dcd62e622e64a16c447443056710cf
lizexiong <zexiong.li@qq.com>
2023-03-23 21:13:55 +0800

    day11 start w10 2023-03-23-21:13:52

---
+++ day11/声明映射.py
@@ -0,0 +1,10 @@
+:wq::wq
+:w：：：；：；：：：·1skljfdsfldskfjsdl;:#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2023/3/23 21:10
+# @Author   : 李泽雄
+# @BoKeYuan : 小家电维修
+# @File     : 声明映射.py
+# @Version  : Python 3.10.10
+# @Project  : python3
+# @Software : PyCharm
