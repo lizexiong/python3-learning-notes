25620daa361420f03e331a052d11696b98b9f96d
lizexiong <zexiong.li@qq.com>
2023-05-05 00:43:18 +0800

    day17 finish w10 2023-05-05-00:43:13

---
+++ day17/冒泡排序.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2023/5/4 19:12
+# @Author   : 李泽雄
+# @BoKeYuan : 小家电维修
+# @File     : 冒泡排序.py
+# @Version  : Python 3.10.10
+# @Project  : python3
+# @Software : PyCharm
+
+
+
+import random
+
+#冒泡,每两个相近的值做对比,每次比相邻的两个值
+
+def bubbling(array):
+    #1.比如数组长度为10，那么循环10次
+    for i in range(len(array)):
+        #2.里面的循环从i开始，第一次是下标array[0]的值
+
+        #5.进入第二次循环,i为1 ，那么j就是1，  那么对比的就是array[1] 和array[1+1]对比，以此类推
+        for j in range(i,len(array)-1):
+            #3.下标array[0]和array[0+1]的值，比较，如果下标0的值大，那么调换位置,把大的值放在后面
+            #4.第二个内层循环 array[1]和array[2]在比较,在把大的值放到后面
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+
+
+if __name__ == "__main__":
+    array = []
+    for i in range(50):
+        array.append(random.randrange(1000))
+        bubbling(array)
+        print (array)
---
+++ day17/选择排序优化版.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time     : 2023/5/4 20:18
+# @Author   : 李泽雄
+# @BoKeYuan : 小家电维修
+# @File     : 选择排序优化版.py
+# @Version  : Python 3.10.10
+# @Project  : python3
+# @Software : PyCharm
+
+import random
+
+#选择排序优化版,记住最大或者最小值的下标,但是值只在外层循环结束后更改
+
+
+def selection_sort(array):
+     #1.比如数组长度为10，那么循环10次
+    for i in range(len(array)):
+        #2.把下标和循环的值生成一个变量
+        small_index = i
+        for j in range(i+1,len(array)):
+            #3.如果 array[small_index](第一次循环等于array[i]) 大于 array[j](第一次循环等于array[i+1])
+            if array[small_index] > array[j]:
+                #4.那么将 small_index设置为j,那么这是array[small_index] = array[j],那么存储的就是最小的值
+                #第二次内层循环进来,那么直接与这个最小值做对比,但是不更改值
+                small_index = j
+        #5.第一个大循环结束之后,array[small_index]存储的就是最小值的下标了,那么再将最小值给array[i]，也就是初始循环的下标
+        temp = array[small_index]
+        array[small_index] = array[i]
+        array[i] = temp
+
+if __name__ == "__main__":
+    array = []
+    for i in range(20):
+        array.append(random.randrange(1000))
+
+    selection_sort(array)
+    print (array)
