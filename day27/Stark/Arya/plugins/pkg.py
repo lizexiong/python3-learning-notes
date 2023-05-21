32429593997221ad30aeb0c9aeb7ecb0fe90ff75
lizexiong <zexiong.li@qq.com>
2023-05-22 03:55:00 +0800

    day27 start w10 2023-5-22-03:54:52

---
+++ day27/Stark/Arya/plugins/pkg.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author:Alex Li
+from Arya.backends.base_module import BaseSaltModule
+
+
+class Pkg(BaseSaltModule):
+
+    pass
+
+    def is_required(self,*args,**kwargs):
+        pass
---
+++ day27/Stark/Robb/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class RobbConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Robb'
