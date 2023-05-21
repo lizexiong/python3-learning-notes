e6e1fad208d7fd05ea397848061d540630a4d641
lizexiong <zexiong.li@qq.com>
2023-05-22 02:23:21 +0800

    所有源码备份 https://github.com/triaquae?page 2

--- day27/__init__.py
+++ day27/__init__.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# @Time     : 2023/5/21 22:01
+# @Time     : 2023/5/21 22:02
 # @Author   : 李泽雄
 # @BoKeYuan : 小家电维修
-# @File     : day26一天什么都没有，和25课代码一起的.py
+# @File     : __init__.py.py
 # @Version  : Python 3.10.10
 # @Project  : python3
 # @Software : PyCharm
-
-
